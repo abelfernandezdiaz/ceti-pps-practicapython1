80589d04185be10db120f9d27612b0e420e72dfd
abelfernandezdiaz <76777350+abelfernandezdiaz@users.noreply.github.com>
2020-12-30 13:14:01 +0100

    Create prpython1.py

---
+++ prpython1.py
@@ -0,0 +1,49 @@
+numAlumnos = 5;
+contador = 0;
+edadIntro = 0;
+edadMax = 0;
+edadMin = 0;
+edadesPares = 0;
+edadesImpares = 0;
+mayorPrimo = 0;
+cuentaPrimo = 0;
+esPrimo = True
+factorial = 1;
+
+while contador < numAlumnos:
+  edadIntro = int(input("Introduce edad: "))
+  if(edadIntro > 0):
+    contador += 1
+    if(edadMax < edadIntro or contador == 1):
+      edadMax = edadIntro
+    if(edadMin > edadIntro or contador == 1):
+      edadMin = edadIntro
+    if edadIntro % 2 == 0:
+      edadesPares += 1
+    else:
+      edadesImpares += 1
+
+    cuentaPrimo = edadIntro - 1
+    esPrimo = True
+    while cuentaPrimo > 1:
+      if edadIntro % cuentaPrimo == 0:
+        esPrimo = False
+      cuentaPrimo -= 1
+    if (esPrimo == True and mayorPrimo < edadIntro):
+      mayorPrimo = edadIntro
+
+
+print ("Edad máxima",edadMax)
+print ("Edad mínima",edadMin)
+print ("Edades impares",edadesImpares)
+print ("Edades pares",edadesPares)
+
+if mayorPrimo > 0:
+  print("Mayor primo:",mayorPrimo)
+  contador = mayorPrimo
+  while contador > 0:
+    factorial = factorial * contador
+    contador -= 1
+  print("El factorial de",mayorPrimo,"es",factorial)
+else:
+  print("No hay números primos")
